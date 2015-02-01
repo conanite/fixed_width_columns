bd72788fe5ef86783df18df27f2bc53aef676173
conanite <conan@conandalton.net>
2015-02-01 23:06:23 +0100

    format: use date format specification when present and when the value is a date

--- lib/fixed_width_columns/fixed_width_attribute.rb
+++ lib/fixed_width_columns/fixed_width_attribute.rb
@@ -4,17 +4,25 @@ class FixedWidthColumns
   class FixedWidthAttribute
     include Aduki::Initializer
     attr_writer :align, :padding
-    attr_accessor :name, :length, :text
+    attr_accessor :name, :length, :text, :date_format
 
     def align   ; (@align || :right).to_sym ; end
     def padding ; @padding || ' '           ; end
+
+    def format_date value
+      return value unless value.is_a? Date
+      return value.to_s unless date_format
+      value.strftime date_format
+    end
 
     def format value, override={ }
       my_length  = override[:length]  || self.length  || value.length
       my_align   = override[:align]   || self.align
       my_padding = override[:padding] || self.padding
 
-      str = value.to_s[0...my_length]
+      str = format_date value
+
+      str = str.to_s[0...my_length]
       case my_align
       when :left
         str.ljust my_length, my_padding
--- spec/format_spec.rb
+++ spec/format_spec.rb
@@ -10,27 +10,29 @@ describe FixedWidthColumns do
     def credit ; (@credit * 100).to_i; end
   end
 
+  def dp str; Date.parse str; end
+
   it "should produce text formatted according to the given specification" do
     format_spec = [
-                   { name: :id       , length:  5                 },
-                   { name: :date     , length: 14                 },
-                   { name: :' '      , text:   ' '                },
-                   { name: :ref      , length: 24, align: :left   },
-                   { name: :debit    , length:  8, padding: '0'   },
-                   { name: :credit   , length:  8, padding: '0'   },
+                   { name: :id       , length:  5                        },
+                   { name: :date     , length:  9, date_format: "%d%m%Y" },
+                   { name: :' '      , text:   ' '                       },
+                   { name: :ref      , length: 24, align: :left          },
+                   { name: :debit    , length:  8, padding: '0'          },
+                   { name: :credit   , length:  8, padding: '0'          },
                   ]
 
-    ft1 = FinancialTransaction.new(id: 1, date: "2003-12-28", ref: "payment thanks", debit:   0.00, credit: 123.45)
-    ft2 = FinancialTransaction.new(id: 2, date: "2004-03-12", ref: "invoice 123"   , debit: 666.66, credit: 0)
-    ft3 = FinancialTransaction.new(id: 3, date: "2005-06-08", ref: "payment thanks", debit:   0.00, credit: 444.44)
-    ft4 = FinancialTransaction.new(id: 4, date: "2006-06-21", ref: "credit note"   , debit:   0.00, credit: 222.22)
+    ft1 = FinancialTransaction.new(id: 1, date: dp("2003-12-28")  , ref: "payment thanks", debit:   0.00, credit: 123.45)
+    ft2 = FinancialTransaction.new(id: 2, date: dp("2004-03-12")  , ref: "invoice 123"   , debit: 666.66, credit: 0)
+    ft3 = FinancialTransaction.new(id: 3, date: "to be determined", ref: "payment thanks", debit:   0.00, credit: 444.44)
+    ft4 = FinancialTransaction.new(id: 4, date: dp("2006-06-21")  , ref: "credit note"   , debit:   0.00, credit: 222.22)
 
     formatter = FixedWidthColumns.new(columns: format_spec)
 
-    expect(formatter.headers).   to eq "   id          date ref                        debit  credit"
-    expect(formatter.format ft1).to eq "    1    2003-12-28 payment thanks          0000000000012345"
-    expect(formatter.format ft2).to eq "    2    2004-03-12 invoice 123             0006666600000000"
-    expect(formatter.format ft3).to eq "    3    2005-06-08 payment thanks          0000000000044444"
-    expect(formatter.format ft4).to eq "    4    2006-06-21 credit note             0000000000022222"
+    expect(formatter.headers).   to eq "   id     date ref                        debit  credit"
+    expect(formatter.format ft1).to eq "    1 28122003 payment thanks          0000000000012345"
+    expect(formatter.format ft2).to eq "    2 12032004 invoice 123             0006666600000000"
+    expect(formatter.format ft3).to eq "    3to be det payment thanks          0000000000044444"
+    expect(formatter.format ft4).to eq "    4 21062006 credit note             0000000000022222"
   end
 end
